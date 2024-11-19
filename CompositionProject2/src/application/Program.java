109c487de3cfcfa9eaebb0492363c065d6782dfa
jvitords <jvduarte.santos@hotmail.com>
2024-11-19 16:05:45 -0300

    Adicionando função de adicionar post e comentários

--- CompositionProject2/src/application/Program.java
+++ CompositionProject2/src/application/Program.java
@@ -10,22 +10,29 @@ import entities.Post;
 public class Program {
 	public static void main(String[] args) {
 		List<Post> postagens = new ArrayList<Post>();
-		adicionarPost(postagens);
+		
+		Post post = new Post();
+		post.adicionarPost(postagens);
+		
+		Post post2 = new Post();
+		post2.adicionarPost(postagens);
+		
+		post.mostrarPostagensFeitas(postagens);
 	}
 	
-	public static void adicionarPost(List<Post> lista) {
+	/*public static void adicionarPost(List<Post> lista) {
 		
 		Scanner digitar = new Scanner(System.in);
 		//List<Post> postagens = new ArrayList<Post>();
 		
-		System.out.println("======== POSTAGEM ========\n");
+		System.out.println("======== CRIAR NOVA POSTAGEM ========\n");
 		
 		System.out.print("Titulo: ");
 		String tituloPostagem =  digitar.nextLine();  
 		System.out.print("Legenda: ");
 		String legendaPostagem =  digitar.nextLine();  
 
-		Post postagem = new Post(LocalDate.now(), "", "", 5);
+		Post postagem = new Post(LocalDate.now(), tituloPostagem, legendaPostagem, null);
 		lista.add(postagem);
-	}
+	}*/
 }
--- CompositionProject2/src/entities/Post.java
+++ CompositionProject2/src/entities/Post.java
@@ -1,6 +1,8 @@
 package entities;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -8,18 +10,42 @@ import java.util.Scanner;
 
 public class Post {
 	
-	private LocalDate moment;
+	private LocalDateTime moment;
 	private String title;
 	private String content; // legenda do post
 	private Integer likes;
 	List<Coment> listaDeComentarios = new ArrayList<Coment>();
 	
-	public Post(LocalDate moment, String title, String content, Integer likes) {
+	public Post() {
+	}
+	
+	public Post(LocalDateTime moment, String title, String content, Integer likes) {
 		this.moment = moment;
 		this.title = title;
 		this.content = content;
 		this.likes = likes;
 	}
+	
+	public void adicionarPost(List<Post> lista) {
+		
+		Scanner digitar = new Scanner(System.in);
+		//List<Post> postagens = new ArrayList<Post>();
+		
+		System.out.println("======== CRIAR NOVA POSTAGEM ========\n");
+		
+		System.out.print("Titulo: ");
+		String tituloPostagem =  digitar.nextLine();  
+		System.out.print("Legenda: ");
+		String legendaPostagem =  digitar.nextLine();  
+		
+		LocalDateTime dataDaPostagem = LocalDateTime.now();
+
+		
+		Post postagem = new Post(dataDaPostagem, tituloPostagem, legendaPostagem, null);
+		lista.add(postagem);
+		//System.out.println("\n" + dataDaPostagem + "\n" + postagem.getTitle() + "\n" + postagem.getContent() + "\n" + postagem.getLikes() + "\n\n");
+	}
+	
 	
 	public void adicionarComentarios(Coment comentario) {
 		listaDeComentarios.add(comentario);
@@ -29,6 +55,12 @@ public class Post {
 	public void removerComentarios(Coment comentario) {
 		listaDeComentarios.remove(comentario);
 		System.out.println("\nComentário retirado com sucesso!\n");
+	}
+	
+	public void mostrarPostagensFeitas(List<Post> listaDePostagem) {
+		for (Post postagem : listaDePostagem) {			
+			System.out.println("\n" + postagem.getMoment() + "\n" + postagem.getTitle() + "\n" + postagem.getContent() + "\n" + postagem.getLikes() + "\n\n");
+		}
 	}
 	
 	public void verComentarios() {
@@ -61,7 +93,9 @@ public class Post {
 		this.likes = likes;
 	}
 
-	public Date getMoment() {
-		return moment;
+	public String getMoment() {
+		DateTimeFormatter formatoData = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+		String dataFormatada = moment.format(formatoData);
+		return dataFormatada;
 	}
 }
